05559614926efe0bd7d7b3008991fdbf87bdcc16
bradkillins <bradkillins@gmail.com>
2021-03-18 21:16:08 -0600

    hooked frontend into db and ipfs

--- Auxiun_wallet/auxiun-wallet/src/components/Content/Items/item-list.js
+++ Auxiun_wallet/auxiun-wallet/src/components/Content/Items/item-list.js
@@ -3,13 +3,15 @@ import Item from "./item";
 
 export default function ItemList() {
   const [items, setItems] = useState([]);
+  //const [reloadList, setReloadList] = useState(false);
 
   useEffect(async () => {
     const fetchedData = await fetch(
-      "http://localhost:3000/api/marketplace/assets"
+      "http://localhost:5000/api/marketplace/assets"
     )
       .then((res) => res.json())
       .then((data) => data);
+    console.log(fetchedData);
     setItems(fetchedData);
   }, []);
 
